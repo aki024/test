c507a4ab847bafb3aefd25386dbcbf8554ce2046
aki024 <aleksa.simicc7@gmail.com>
2024-01-22 09:55:56 +0100

    test

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
-
+import { NavigationEvents, RouteChangeListener } from '@/components/route-change';
 import { Suspense } from 'react';
-import NavigationEvents from './NavigationEvents';
+import Script from 'next/script';
 
 export const metadata = {
 	title: 'NextLevel Food',
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
 		<html lang='en'>
 			<body>
 				<MainHeader />
+				<Script src='http://localhost:8080/target/target-script-min.js#anonymous' />
 				<Suspense fallback={null}>
 					<NavigationEvents />
 				</Suspense>

a0813a82eb61800f3cc2015242fd6a64780afaf2
aki024 <aleksa.simicc7@gmail.com>
2024-01-22 10:04:00 +0100

    ss

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
-import { NavigationEvents, RouteChangeListener } from '@/components/route-change';
+// import { NavigationEvents, RouteChangeListener } from '@/components/route-change';
 import { Suspense } from 'react';
 import Script from 'next/script';
 
@@ -15,9 +15,7 @@ export default function RootLayout({ children }) {
 			<body>
 				<MainHeader />
 				<Script src='http://localhost:8080/target/target-script-min.js#anonymous' />
-				<Suspense fallback={null}>
-					<NavigationEvents />
-				</Suspense>
+				<Suspense fallback={null}>{/* <NavigationEvents /> */}</Suspense>
 
 				{children}
 			</body>
